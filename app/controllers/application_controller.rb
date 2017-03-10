038506fb3d9b60868a45b0ea5707077dc6f0bcf4
Grzegorz Milanowski <grzegorz.od.tych.milanowskich@gmail.com>
2017-03-10 20:37:42 +0100

    Add login with github

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user
 
-  def hello
-    render html: 'hello world'
+  def current_user
+    session[:user_id].nil? ? nil : User.find(session[:user_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  get "/auth/:provider/callback", to: "sessions#create"
+  get 'auth/failure', to: redirect('/')
+  delete 'signout', to: 'sessions#destroy', as: 'signout'
+  root to: 'sessions#new'
 end
